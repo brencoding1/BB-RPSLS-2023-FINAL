84604132b0d2709455a4400a069ccaa40a1a77e4
brencoding1 <brencoding1@gmail.com>
2023-08-16 04:06:59 +0000

    added funsctions to rock paper and scissor

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,10 @@ const game = () => {
     let playerScore = 0;
     let computerScore = 0;
     let moves = 0;
+    // Function to
+const playGame = () => {
+    const rockBtn = document.querySelector('.rock');
+    const paperBtn = document.querySelector('.paper');
+    const scissorBtn = document.querySelector('.scissor');
+    const playerOptions = [rockBtn,paperBtn,scissorBtn];
+    const computerOptions = ['rock','paper','scissors']    
